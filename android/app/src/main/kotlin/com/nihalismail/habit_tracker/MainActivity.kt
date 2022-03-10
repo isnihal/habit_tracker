7e154e774b3a3ce0e53edbb6848dcf2b83ba8d37
Nihal Ismail <nihalismailk@gmail.com>
2022-03-10 11:52:34 -0500

    Project init

---
+++ android/app/src/main/kotlin/com/nihalismail/habit_tracker/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.nihalismail.habit_tracker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:habit_tracker/constants/app_assets.dart';
+import 'package:habit_tracker/constants/app_colors.dart';
+import 'package:habit_tracker/ui/home/home_page.dart';
+import 'package:habit_tracker/ui/theming/app_theme.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await AppAssets.preloadSVGs();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'Helvetica Neue',
+      ),
+      home: AppTheme(
+        data: AppThemeData.defaultWithSwatch(AppColors.red),
+        child: HomePage(),
+      ),
+    );
+  }
+}
